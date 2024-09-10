820c48fe6baa7e21a094d5b6a706fb1a77290c1d
Yoonjeong Ko <jejukyj@naver.com>
2024-09-10 13:53:47 +0900

    [#design] 게시글 페이지 기본 CSS 작성

--- src/pages/main/Post.jsx
+++ src/pages/main/Post.jsx
@@ -5,7 +5,20 @@ const Post = () => {
     return (
         <>
             <Wrapper>
-                게시글 자세히보기
+                <section>
+                    <button></button>
+                </section>
+                <Image className='photo'>
+                    <img src="" alt="첨부한 사진" />
+                </Image>
+                <section>
+                <h2>평양냉면 도장깨기 함께 하실 분</h2>
+                <div>
+                    <span>작성자</span> <span>고윤정</span> <span>프론트엔드/대면</span>
+                </div>
+                    <span>모집기간 24.09.05 17:00 ~ 24.09.09 18:00</span>
+                </section>
+                <div></div>
             </Wrapper>
         </>
     );
@@ -19,5 +32,10 @@ const Wrapper = styled.div`
     
 `;
 
-
+const Image = styled.div`
+  width: 326px;
+  height: 326px;
+  background-color: gray;
+  border-radius: 11px;
+`;
 
