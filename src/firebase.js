751e5421f30590e14678e97a24a139c9d2025089
jueunseuk <junsu120202@gmail.com>
2024-09-11 16:42:22 +0900

    [#feature] 파이어베이스 초기 설정 및 게시글 작성 구현 완료

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// firebase 설정 파일
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+// 변수 app을 생성하고 설정값으로 초기화
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// firestore 객체 생성
+const db = getFirestore(app);
+
+// firestore exort
+export {db};
