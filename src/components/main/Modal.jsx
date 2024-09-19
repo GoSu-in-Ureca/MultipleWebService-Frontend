823cb90b9f52e489c42b17409efa6669f9821e5c
Yoonjeong Ko <jejukyj@naver.com>
2024-09-19 15:45:29 +0900

    [#feature] 모달 컴포넌트 추가

---
+++ src/components/main/Modal.jsx
@@ -0,0 +1,79 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+// isOpen : 모달 열려있는지 여부
+// onClose : 모달 닫기
+// children : 모달 안에 들어갈 내용
+
+const Modal = ({isOpen, onClose, modalPosition}) => {
+  const navigate = useNavigate();
+
+  if(!isOpen) return null;
+
+  const handleUpdateClick = () => {
+    navigate('/update');
+}
+
+  return(
+    <ModalOverlay onClick={onClose}>
+      <ModalBox
+        style={{
+          position: "absolute",
+          top: `${modalPosition.top}px`,
+          left: `${modalPosition.left}px`,
+        }} 
+        onClick={(e)=>e.stopPropagation()}>
+        {/* children */}
+        <UpdateButton onClick={handleUpdateClick}>수정</UpdateButton>
+        <DelButton>삭제</DelButton>
+        <EndButton>모집 마감</EndButton>
+      </ModalBox>
+    </ModalOverlay>
+  );
+};
+
+export default Modal;
+
+const ModalOverlay = styled.div`
+  /* visibility: hidden; */
+   position : fixed;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 100%;
+   background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+const ModalBox = styled.div`
+  background: white;
+  border-radius: 8px;
+  box-shadow: inset;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const UpdateButton = styled.div`
+  cursor: pointer;
+  border: 0;
+  padding: 15px 15px 10px 15px;
+`;
+
+const DelButton = styled.div`
+  cursor: pointer;
+  border: 0;
+  padding: 10px 20px 10px 20px;
+  border-top: 1px solid #e9e9e9;
+  border-bottom: 1px solid #e9e9e9;
+`;
+
+const EndButton = styled.div`
+  cursor: pointer;
+  border: 0;
+  padding: 10px 15px 15px 15px;
+`;
